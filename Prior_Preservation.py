38c03d6229d7bb8f6353c0fcc0d9bb21703f4f5b
DaniDapena <50242217+DaniDapena@users.noreply.github.com>
2022-10-28 14:53:51 -0700

    app

---
+++ Prior_Preservation.py
@@ -0,0 +1,76 @@
+
+import torch
+from diffusers import StableDiffusionPipeline
+from tqdm.auto import tqdm
+from datasets import PromptDataset
+from pathlib import Path
+import gc
+import lightning as L
+from lightning_app.storage import Drive
+import os
+
+
+def get_filenames_of_path(path_s):
+        """Returns a list of files in a directory/path. Uses pathlib."""
+        filenames = [path_s+"/"+path for path in os.listdir(path_s)]
+        return filenames
+
+
+
+class Prior(L.LightningWork):  
+    def __init__(self,drive,cloud_compute, *args, **kwargs):
+        super().__init__(*args, cloud_compute=cloud_compute, **kwargs)
+        self.drive_1 = drive
+        self.drive_2 = Drive("lit://drive_2")
+
+    def run(self, class_prompt, YOUR_TOKEN="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",sample_batch_size = 2): 
+        
+        # type of devie
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        # number of examples
+        num_class_images =  len((self.drive_1.list()))
+        cur_class_images = len((self.drive_2.list()))
+
+        # create examples prior
+        if cur_class_images < num_class_images:
+            ## This need to change from hugging face
+            pipeline = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4",use_auth_token=YOUR_TOKEN
+            ).to(device)
+
+            # number of examples to generate
+            num_new_images = num_class_images - cur_class_images
+            print(f"Number of class images to sample: {num_new_images}.")
+
+
+
+            sample_dataset = PromptDataset(class_prompt, num_new_images)
+            # this is to get the description of the images
+            sample_dataloader = torch.utils.data.DataLoader(sample_dataset, batch_size=sample_batch_size)
+
+            # generate the examples
+            for example in tqdm(sample_dataloader, desc="Generating class images"):
+                # example genration
+                images = pipeline(example["prompt"])
+                # get image
+                images = images["sample"]
+                
+                # save images
+                for i, image in enumerate(images):
+                    image.save( f"{example['index'][i] + cur_class_images}.jpg")
+                    print(f"{example['index'][i] + cur_class_images}.jpg")
+                    self.drive_2.put(f"{example['index'][i] + cur_class_images}.jpg")
+
+
+            pipeline = None
+            gc.collect()
+            del pipeline
+            with torch.no_grad():
+                torch.cuda.empty_cache()
+
+
+
+
+
+
+      
---
+++ app.py
@@ -0,0 +1,79 @@
+import lightning as L
+#from quick_start.components import  ImageServeGradio, PLTracerPythonScript
+
+import warnings
+warnings.simplefilter("ignore")
+from create_data import Create_Data
+import os
+#from Prior_Preservation import Prior
+from argparse import Namespace
+#from models import Training
+import torch
+
+
+
+class TrainDeploy(L.LightningFlow):
+    def __init__(self):
+        super().__init__()
+
+        # work that gets the data
+        self.create_data = Create_Data()
+
+        # # work to generate prior
+        # self.prior =  Prior(self.create_data.drive_1, cloud_compute=L.CloudCompute("gpu"))
+
+        #  # work that trains my model
+        # self.train_work = Training(cloud_compute=L.CloudCompute("gpu"))
+
+    def run(self):
+         # Download pictures of my concept
+        url = "https://drive.google.com/drive/folders/1PzdEZ0u87yxoy5cAMk12tJe0gE2LtYUA?usp=sharing"
+        self.create_data.run(url=url)
+
+    #     # Create previous  examples
+    #     self.prior.run("a photo of a Daniela person")
+
+        
+    # # arguments for training
+    #     args = Namespace(
+    #         # Model Download Folder
+    #                     pretrained_model_name_or_path="CompVis/stable-diffusion-v1-4" ,
+    #         # image Resolution
+    #                     resolution=512,
+    #         # Image transformation
+    #                     center_crop=True,
+    #         # Folder of my concept
+    #                     instance_data_dir= self.create_data.drive_1,
+    #         # My concept text prompt
+    #                     instance_prompt="a photo of Daniela person" ,
+    #         # learning rate
+    #                     learning_rate=5e-06,
+    #         # batch size
+    #                     train_batch_size=1,
+    #         # use 8bit optimizer from bitsandbytes
+    #                     use_8bit_adam=True, 
+    #         # Prior preservation
+    #                     with_prior_preservation=True, 
+    #         # Prior weight
+    #                     prior_loss_weight=1,
+    #         # Prior folder
+    #                     class_data_dir=self.prior.drive_2, 
+    #         # prior text prompt
+    #                     class_prompt="a photo of a sks person", 
+    #         # number of examples
+    #                     num_class_images=self.create_data.number_samples, 
+    #         # folder to store re-trained mode;
+    #                     output_dir="dreambooth-concept",
+    #         # num epochs
+    #                     num_epochs = 1)
+
+
+    #     # run training
+    #     self.train_work.run(args)
+        
+    # def configure_layout(self):
+    #     tab_1 = {"name": "Model training", "content": self.train_work}
+    #     return [tab_1]#, tab_2]
+
+app = L.LightningApp(TrainDeploy())
+

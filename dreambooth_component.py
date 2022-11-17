b776d557451c77cdf3108bc88612ce895aa417a4
hhsecond <sherin@grid.ai>
2022-11-17 10:25:11 +0100

    clean up

--- dreambooth_component.py
+++ dreambooth_component.py
@@ -1,12 +1,13 @@
+from lightning.app.storage import Path
+
 from base_diffusion import BaseDiffusion
 import base64
 from io import BytesIO
-from typing import Any
 
 from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer
 from diffusers import AutoencoderKL, DDPMScheduler, StableDiffusionPipeline, UNet2DConditionModel
-from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 
+from diffusion_serve import DreamBoothInput
 
 PRETRAINED_MODEL_NAME = "CompVis/stable-diffusion-v1-4"
 HF_TOKEN = "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"
@@ -19,12 +20,13 @@ class DreamBooth(BaseDiffusion):
         vae = AutoencoderKL.from_pretrained(PRETRAINED_MODEL_NAME, subfolder="vae", use_auth_token=HF_TOKEN)
         tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
         scheduler = DDPMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
-        # safety_checker = StableDiffusionSafetyChecker.from_pretrained("CompVis/stable-diffusion-safety-checker")
         safety_checker = None
         feature_extractor = CLIPFeatureExtractor.from_pretrained("openai/clip-vit-base-patch32")
 
         model_path = kwargs.get("checkpoint_path")
         if model_path:
+            if not isinstance(model_path, Path):
+                raise ValueError("checkpoint_path must be a lightning.storage.Path object")
             model_path.get()
             unet_path = model_path
         else:
@@ -41,12 +43,12 @@ class DreamBooth(BaseDiffusion):
             feature_extractor=feature_extractor
         )
 
-    def predict(self, data: Any):
+    def predict(self, data: DreamBoothInput):
         prompt = data.prompt
         if data.quality == "low":
             num_steps = 1
         elif data.quality == "medium":
-            num_steps = 5
+            num_steps = 10
         elif data.quality == "high":
             num_steps = 50
         else:
@@ -54,6 +56,7 @@ class DreamBooth(BaseDiffusion):
         out = self._model(prompt=prompt, num_inference_steps=num_steps)
         images = out[0]
         image = images[0]
+        image.save("image-ref.png")
         buffered = BytesIO()
         image.save(buffered, format="PNG")
         return {"image": base64.b64encode(buffered.getvalue())}

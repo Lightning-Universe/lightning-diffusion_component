34c80ae7ccd9248b91e666e28d5adec9bb07a19f
Thomas Chaton <thomas@grid.ai>
2022-11-17 11:40:34 -0500

    update

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
-from dreambooth import DreamBoothFineTuner
+from dreambooth import DreamBoothTuner
 import lightning as L
+from diffusers import StableDiffusionPipeline
+import models
+from lightning.app.storage import Drive
 
-app = L.LightningApp(
-    DreamBoothFineTuner(
-        image_urls = [
-            "https://huggingface.co/datasets/valhalla/images/resolve/main/2.jpeg",
-            "https://huggingface.co/datasets/valhalla/images/resolve/main/3.jpeg",
-            "https://huggingface.co/datasets/valhalla/images/resolve/main/5.jpeg",
-            "https://huggingface.co/datasets/valhalla/images/resolve/main/6.jpeg",
-            ## You can change or add additional images here
-        ],
-        prompt="a photo of sks toy", # `sks` is the special name
-        preservation_prompt="a photo of a cat clay toy",
-        learning_rate=5e-06,
-        max_steps=50,
-        cloud_compute=L.CloudCompute("gpu-fast"),
-    )
-)
+class DreamBooth(L.LightningWork):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.weights_drive = Drive("lit://weights")
+
+    def setup(self):
+        self._model = StableDiffusionPipeline(
+            text_encoder=models.create_text_encoder(),
+            vae=models.create_vae(),
+            unet=models.create_unet(self.weights_drive),
+            tokenizer=models.create_tokenizer(),
+            scheduler=models.create_scheduler(),
+            safety_checker=models.create_safety_checker(),
+            feature_extractor=models.create_feature_extractor(),
+        )
+
+    def finetune(self):
+        DreamBoothTuner(
+            image_urls=[
+                "https://huggingface.co/datasets/valhalla/images/resolve/main/2.jpeg",
+                "https://huggingface.co/datasets/valhalla/images/resolve/main/3.jpeg",
+                "https://huggingface.co/datasets/valhalla/images/resolve/main/5.jpeg",
+                "https://huggingface.co/datasets/valhalla/images/resolve/main/6.jpeg",
+                ## You can change or add additional images here
+            ],
+            prompt="a photo of [sks] [cat clay toy] [riding a bicycle]",
+        ).run(self._model)
+
+    def run(self):
+        self.setup()
+        self.finetune()
+
+
+app = L.LightningApp(DreamBooth())
--- models.py
+++ models.py
@@ -1,33 +1,35 @@
 from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer
 from diffusers import AutoencoderKL, DDPMScheduler, UNet2DConditionModel
 from lightning.app.storage import Drive
+from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
+
 
 HF_TOKEN = "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"
+pretrained_model_name_or_path = "CompVis/stable-diffusion-v1-4"
 
 
 def create_text_encoder():
-    return CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
-
+    return CLIPTextModel.from_pretrained(pretrained_model_name_or_path, subfolder="text_encoder", use_auth_token=HF_TOKEN)
 
 def create_vae():
-    return AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", use_auth_token=HF_TOKEN),
+    return AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", use_auth_token=HF_TOKEN)
 
 def create_unet(drive: Drive):
-    source = "CompVis/stable-diffusion-v1-4"
     if drive.list() == ["model.pt"]:
         drive.get("model.pt")
         source = "model.pt"
-    return UNet2DConditionModel.from_pretrained(source, subfolder="unet", use_auth_token=HF_TOKEN),
+    else:
+        source = pretrained_model_name_or_path
+    return UNet2DConditionModel.from_pretrained(source, subfolder="unet", use_auth_token=HF_TOKEN)
 
 def create_tokenizer():
-    return CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+    return CLIPTokenizer.from_pretrained(pretrained_model_name_or_path, subfolder="tokenizer", use_auth_token=HF_TOKEN)
 
 def create_scheduler():
-    return DDPMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
+    return DDPMScheduler.from_config(pretrained_model_name_or_path, subfolder="scheduler")
 
 def create_feature_extractor():
     return CLIPFeatureExtractor.from_pretrained("openai/clip-vit-base-patch32")
 
-
 def create_safety_checker():
-    return None
+    return StableDiffusionSafetyChecker.from_pretrained("CompVis/stable-diffusion-safety-checker")

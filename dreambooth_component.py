c8e5983d30fd44790d7f5df2b475834a2ed9ac71
hhsecond <sherin@grid.ai>
2022-11-17 20:18:40 +0100

    clean up

--- dreambooth_component.py
+++ dreambooth_component.py
@@ -3,8 +3,7 @@ import torch.cuda
 
 from base_diffusion import BaseDiffusion
 
-from transformers import CLIPFeatureExtractor, CLIPTextModel, CLIPTokenizer
-from diffusers import AutoencoderKL, DDPMScheduler, StableDiffusionPipeline, UNet2DConditionModel
+from diffusers import StableDiffusionPipeline
 from diffusion_serve import DreamBoothInput
 from utils import image_decode
 
@@ -16,8 +15,10 @@ class DreamBooth(BaseDiffusion):
 
     def setup(self, *args, **kwargs):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        StableDiffusionPipeline.from_pretrained()
-        self._model = model
+        self._model = StableDiffusionPipeline.from_pretrained(
+            "CompVis/stable-diffusion-v1-4",
+            **models.extras
+        )
 
     def predict(self, data: DreamBoothInput):
         print("Predicting...")
@@ -27,3 +28,33 @@ class DreamBooth(BaseDiffusion):
 
 
 app = L.LightningApp(DreamBooth())
+
+#
+# import lightning as L
+# import base64, io, models, base_diffusion
+# from diffusers import StableDiffusionPipeline
+#
+#
+# class DreamBoothDiffusion(base_diffusion.BaseDiffusion):
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.weights_drive = L.storage.Drive("lit://weights")
+#
+#     def serialize(self, image):
+#         buffered = io.BytesIO()
+#         image.save(buffered, format="PNG")
+#         return base64.b64encode(buffered.getvalue()).decode("utf-8")
+#
+#     def setup(self):
+#         self.model = StableDiffusionPipeline.from_pretrained(
+#             "CompVis/stable-diffusion-v1-4",
+#             **models.extras
+#         )
+#
+#     def predict(self, data):
+#         images = self.model(prompt=data.prompt, num_inference_steps=1)[0]
+#         return {"images": self.serialize(images)}
+#
+#
+# app = L.LightningApp(DreamBoothDiffusion())

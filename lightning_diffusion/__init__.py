616a804a6475e0262ead214ca00f145d03637526
thomas chaton <thomas@grid.ai>
2022-11-24 12:56:34 +0000

    update

--- lightning_diffusion/__init__.py
+++ lightning_diffusion/__init__.py
@@ -2,11 +2,6 @@ from lightning_diffusion import models
 from lightning_diffusion.base_diffusion import BaseDiffusion
 from lightning_diffusion.diffusion_serve import DreamBoothInput
 from lightning_diffusion.dreambooth import DreamBoothTuner
-<<<<<<< HEAD
 from lightning_diffusion.model_cloud.cloud_api import download_from_lightning_cloud
 
 __all__ = ["BaseDiffusion", "DreamBoothInput", "DreamBoothTuner", "DefaultSafetyFilter", "models", "download_from_lightning_cloud"]
-=======
-
-__all__ = ["BaseDiffusion", "DreamBoothInput", "DreamBoothTuner", "DefaultSafetyFilter", "models"]
->>>>>>> main

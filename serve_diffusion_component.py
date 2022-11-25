72b512782f4c79568ce963bc185db14acd9c20d8
Thomas Chaton <thomas@grid.ai>
2022-11-25 10:52:07 -0500

    update

--- serve_diffusion_component.py
+++ serve_diffusion_component.py
@@ -1,20 +1,16 @@
 # ! pip install torch diffusers git+https://github.com/Lightning-AI/lightning-diffusion-component.git
+import diffusers
 import lightning as L
-from lightning_diffusion import BaseDiffusion
-from diffusers import DiffusionPipeline, EulerDiscreteScheduler
-
+from lightning_diffusion import BaseDiffusion, download_from_lightning_cloud
 
 class ServeDiffusion(BaseDiffusion):
     def setup(self, *args, **kwargs):
-        import torch
-        repo_id = "stabilityai/stable-diffusion-2"
-        scheduler = EulerDiscreteScheduler.from_pretrained(repo_id, subfolder="scheduler", prediction_type="v_prediction")
-        self.model = DiffusionPipeline.from_pretrained(repo_id, torch_dtype=torch.float32, revision="fp16", scheduler=scheduler)
-        # self.model = model.to(self.device)
+        download_from_lightning_cloud("daniela/stable_diffusion", version="latest", output_dir="model")
+        self.model = diffusers.StableDiffusionPipeline.from_pretrained("model").to(self.device)
 
     def predict(self, data):
         out = self.model(prompt=data.prompt, num_inference_steps=23)
         return {"image": self.serialize(out[0][0])}
 
 
-app = L.LightningApp(ServeDiffusion(interactive=True))
+app = L.LightningApp(ServeDiffusion())

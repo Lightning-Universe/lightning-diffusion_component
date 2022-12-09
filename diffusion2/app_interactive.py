fe4a294d45a510295f247b22463a323eaa54cf64
thomas <thomas@thomass-MacBook-Pro.local>
2022-12-09 16:00:28 +0000

    update

--- diffusion2/app_interactive.py
+++ diffusion2/app_interactive.py
@@ -9,14 +9,9 @@ from io import BytesIO
 from typing import Any, Callable, Optional
 from ldm.lightning import LightningStableDiffusion, PromptDataset
 from nicegui import ui
-from pydantic import BaseModel
+import openai
 
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-
-
-class Text(BaseModel):
-    text: Optional[str]
-
 
 async def io_bound(callback: Callable, *args: Any, **kwargs: Any):
     return await asyncio.get_event_loop().run_in_executor(
@@ -25,8 +20,18 @@ async def io_bound(callback: Callable, *args: Any, **kwargs: Any):
 
 
 def webpage(
-    predict_fn: Callable, host: str, port: int, reference_inference_time: Optional[float], source
+    predict_fn: Callable, host: str, port: int, reference_inference_time: Optional[float], source: Optional[str]
 ):    
+
+    async def enhance_prompt():
+        openai.api_key = key_input.value
+        try:
+            value = str(prompt.value)
+            value = f"Describe the following '{value}' with details"
+            response = openai.Completion.create(model="text-davinci-003", prompt=value, max_tokens=80, temperature=0.7)
+        except openai.error.AuthenticationError:
+            return
+        prompt.value = response["choices"][0]["text"]
 
     async def progress_tracker():
         if progress.value >= 1.0 or progress.value == 0:
@@ -40,7 +45,7 @@ def webpage(
         image.source = (
             "https://dummyimage.com/600x400/ccc/000000.png&text=building+image..."
         )
-        prediction = await io_bound(predict_fn, request=Text(text=prompt.value))
+        prediction = await io_bound(predict_fn, text=prompt.value)
         image.source = prediction["image"]
         progress.value = 1.0
         reference_inference_time = (time.time() - t0)
@@ -50,12 +55,18 @@ def webpage(
         with ui.column():
             ui.label("Stable Diffusion 2.0 with Lightning AI").classes("text-2xl")
             prompt = ui.input("prompt").style("width: 50em")
-            prompt.value = "Woman painting a large red egg in a dali landscape"
-            ui.button("Generate", on_click=generate_image).style("width: 15em")
+            prompt.value = "a dragon with wings made of fire"
+            with ui.row().style("gap:10em"):
+                expander = ui.expansion('Enhance').style("width: 15em")
+                with expander:
+                    key_input = ui.input("Enter your OpenAI Key").style("width: 50em")
+                    ui.button("Enhance", on_click=enhance_prompt).style("width: 15em")
+                ui.button("Generate", on_click=generate_image).style("width: 15em")
             progress = ui.linear_progress()
             ui.timer(interval=0.1, callback=progress_tracker)
             image = ui.image().style("width: 60em")
-            image.source = source
+            if source:
+                image.source = source
 
     # Note: Hack to enable running in spawn context.
     def stack_patch():
@@ -96,10 +107,10 @@ class DiffusionServeInteractive(L.LightningWork):
             self._model = self._model.to(torch.float16)
             torch.cuda.empty_cache()
 
-    def predict(self, request):
+    def predict(self, text):
         image = self._trainer.predict(
             self._model,
-            torch.utils.data.DataLoader(PromptDataset([request.text])),
+            torch.utils.data.DataLoader(PromptDataset([text])),
         )[0][0]
         buffer = BytesIO()
         image.save(buffer, format="PNG")
@@ -109,7 +120,7 @@ class DiffusionServeInteractive(L.LightningWork):
     def run(self):
         self.setup()
         t0 = time.time()
-        image = self.predict(Text(text='Woman painting a large red egg in a dali landscape'))["image"]
+        image = self.predict('a dragon with wings made of fire')["image"]
         webpage(self.predict, self.host, self.port, time.time() - t0, image)
 
 

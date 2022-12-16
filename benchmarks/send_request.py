c9f506cd69adcc09d05a945ac9b302309b0e7e85
Akihiro Nitta <nitta@akihironitta.com>
2022-12-16 20:09:35 +0900

    Update send_request.py

--- benchmarks/send_request.py
+++ benchmarks/send_request.py
@@ -1,12 +1,8 @@
 import requests, PIL.Image as Image, io, base64, time
 
-# url = "https://xskcq-01gmcqx8r148q461b5gwr5cmgt.litng-ai-03.litng.ai/predict"
-url = "https://zsvqr-01gmcs3cezp37ps00ecxrfnb45.litng-ai-03.litng.ai/predict"
+url = "https://tmmqd-01gmd92jc3gfn1cvwasjz61s7c.litng-ai-03.litng.ai/predict"
 response = requests.post(url, json={
   "text": "Woman painting a large red egg in a dali landscape"
 })
-
-print(response.json()["image"][:100])  # for debugging, check the first 100 chars of the image
-
 image = Image.open(io.BytesIO(base64.b64decode(response.json()["image"])))
 image.save(f"response_{time.time()}.png")
